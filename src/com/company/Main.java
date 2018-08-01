c10598238b2286e2213d172c0b297cc8810a8b26
kasturipratik <pratik.parajuli@yahoo.com>
2018-08-01 13:49:44 -0400

    added ascending and descending order sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int num [] = {1,2,10,11,15,25,2,3,5,7,12,100,17,50,65,85,81,55,3};
+
+        System.out.println("** Bubble Sort ascending order **");
         bubbleSort(num);
+        for(int i =0; i<num.length;i++){
+            System.out.print(num[i] +" ");
+        }
+
+        System.out.println("\n\n** Bubble Sort descending order **");
+        bubbleSortDesc(num);
         for(int i =0; i<num.length;i++){
             System.out.print(num[i] +" ");
         }
@@ -21,6 +29,22 @@ public class Main {
 
             for(int j =1; j < count; j++){
                 if(num[j-1] > num [j]){
+
+                    swap(j-1,j,num);
+
+                }
+            }
+        }
+    }
+
+    public static void bubbleSortDesc(int [] num){
+
+        int count = num.length;
+        int temp =0;
+        for(int i =0; i < count ;i ++){
+
+            for(int j =1; j < count; j++){
+                if(num[j-1] < num [j]){
 
                     swap(j-1,j,num);
 
